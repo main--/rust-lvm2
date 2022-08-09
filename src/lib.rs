7a7339077b34cb997beef88429b0c8f4e63e584e
main() <main@ehvag.de>
2022-08-09 12:52:42 +0200

    jobs done

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@
 extern crate alloc;
 
 use core::marker::PhantomData;
+use core::ops::Range;
 
-use acid_io::{Read, Seek};
+use acid_io::{Read, Seek, SeekFrom};
 use alloc::borrow::ToOwned;
 use alloc::collections::BTreeMap;
 use alloc::string::{String, ToString};
@@ -12,7 +13,7 @@ use nom::IResult;
 use nom::branch::alt;
 use nom::bytes::complete::{take, take_until, take_till1};
 use nom::bytes::streaming::tag;
-use nom::character::complete::{char, multispace0, multispace1, one_of, line_ending, not_line_ending};
+use nom::character::complete::{char, multispace0, line_ending, not_line_ending};
 use nom::combinator::map;
 use nom::error::ParseError;
 use nom::multi::{many_till, many0, separated_list0};
@@ -21,15 +22,18 @@ use nom::number::streaming::le_u64;
 use nom::sequence::{preceded, delimited, tuple};
 use serde::de::value::StrDeserializer;
 use serde::{Deserializer, de, forward_to_deserialize_any, Deserialize};
-use snafu::{Snafu, ResultExt, ensure};
+use snafu::{Snafu, ResultExt, ensure, OptionExt};
 
 // Vocabulary: in this crate we use the term "sheet" to describe a block of exactly 512 bytes
 // (to avoid confusion around the word "sector")
 
-// spec: https://github.com/libyal/libvslvm/blob/main/documentation/Logical%20Volume%20Manager%20(LVM)%20format.asciidoc#2-physical-volume-label
+// spec: https://github.com/libyal/libvslvm/blob/ab09a380072448d9c84c886d487d8c3dfa2d1527/documentation/Logical%20Volume%20Manager%20(LVM)%20format.asciidoc#2-physical-volume-label
 
-pub struct Lvm2<T> {
-    reader: T,
+pub struct Lvm2 {
+    pvh: PhysicalVolumeHeader,
+    pv_name: String,
+    vg_name: String,
+    vg_config: MetadataRoot,
 }
 
 #[derive(Debug, Snafu)]
@@ -37,6 +41,10 @@ pub enum Error {
     Io { source: acid_io::Error },
     WrongMagic,
     ParseError { error: String },
+    MultipleVGsError,
+    PVDoesntContainItself,
+    Serde { source: serde::de::value::Error },
+    MissingMetadata,
 }
 
 #[derive(Debug)]
@@ -150,21 +158,21 @@ impl MetadataValue {
     }
 }
 #[derive(Debug)]
-struct MetadataElements(Vec<MetadataElement>);
-impl MetadataElements {
-    pub fn parse(input: &str) -> IResult<&str, Self> {
+struct MetadataElements<'a>(Vec<MetadataElement<'a>>);
+impl<'a> MetadataElements<'a> {
+    pub fn parse(input: &'a str) -> IResult<&'a str, Self> {
         map(many0(MetadataElement::parse), MetadataElements)(input)
     }
 }
 
 #[derive(Debug)]
-enum MetadataElement {
+enum MetadataElement<'a> {
     Group {
-        name: String,
-        contents: MetadataElements,
+        name: &'a str,
+        contents: MetadataElements<'a>,
     },
     Value {
-        name: String,
+        name: &'a str,
         value: MetadataValue,
     },
 }
@@ -179,27 +187,27 @@ fn whitespace(input: &str) -> IResult<&str, ()> {
     let (input, _) = multispace0(input)?;
     Ok((input, ()))
 }
-impl MetadataElement {
-    pub fn name(&self) -> &str {
+impl<'a> MetadataElement<'a> {
+    pub fn name(&self) -> &'a str {
         match self {
             MetadataElement::Group { name, .. } => &name,
             MetadataElement::Value { name, .. } => &name,
         }
     }
-    pub fn parse(input: &str) -> IResult<&str, Self> {
+    pub fn parse(input: &'a str) -> IResult<&'a str, Self> {
         delimited(
             whitespace,
             alt((
-                map(tuple((parse_ident, whitespace, char('='), whitespace, MetadataValue::parse)), |(k, _, _, _, v)| MetadataElement::Value { name: k.to_owned(), value: v }),
+                map(tuple((parse_ident, whitespace, char('='), whitespace, MetadataValue::parse)), |(name, _, _, _, v)| MetadataElement::Value { name, value: v }),
 
-                map(tuple((parse_ident, whitespace, char('{'), MetadataElements::parse, char('}'))), |(k, _, _, contents, _)| MetadataElement::Group { name: k.to_owned(), contents })
+                map(tuple((parse_ident, whitespace, char('{'), MetadataElements::parse, char('}'))), |(name, _, _, contents, _)| MetadataElement::Group { name, contents })
             )),
             whitespace
         )(input)
     }
 }
 
-impl<'de, 'a> de::Deserializer<'de> for &'a MetadataElements {
+impl<'de, 'a> de::Deserializer<'de> for &'a MetadataElements<'de> {
     forward_to_deserialize_any! {
         bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
         bytes byte_buf option unit unit_struct newtype_struct seq tuple
@@ -214,9 +222,9 @@ impl<'de, 'a> de::Deserializer<'de> for &'a MetadataElements {
         visitor.visit_map(MetadataElementsAccess(self.0.iter().peekable()))
     }
 }
-struct MetadataElementsAccess<'a>(core::iter::Peekable<alloc::slice::Iter<'a, MetadataElement>>);
+struct MetadataElementsAccess<'a, 'de>(core::iter::Peekable<alloc::slice::Iter<'a, MetadataElement<'de>>>);
 
-impl<'de, 'a> de::MapAccess<'de> for MetadataElementsAccess<'a> {
+impl<'de, 'a> de::MapAccess<'de> for MetadataElementsAccess<'a, 'de> {
     type Error = serde::de::value::Error;
 
     fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error>
@@ -271,9 +279,9 @@ impl<'de, 'a> de::SeqAccess<'de> for MetadataValuesAccess<'a> {
     }
 }
 
-impl<T: Read + Seek> Lvm2<T> {
-    pub fn open(mut reader: T) -> Result<Self, Error> {
-        reader.seek(acid_io::SeekFrom::Start(512)).context(IoSnafu)?; // skip zero sheet
+impl Lvm2 {
+    pub fn open<T: Read + Seek>(mut reader: T) -> Result<Self, Error> {
+        reader.seek(SeekFrom::Start(512)).context(IoSnafu)?; // skip zero sheet
 
         let mut buf = [0u8; 512];
         reader.read_exact(&mut buf).context(IoSnafu)?; // read header
@@ -284,40 +292,159 @@ impl<T: Read + Seek> Lvm2<T> {
         let (_, pvh) = PhysicalVolumeHeader::parse(&buf[(vhl.data_offset as usize)..]).map_err(|e| Error::ParseError { error: e.to_string() })?;
         tracing::trace!(?pvh);
 
-        reader.seek(acid_io::SeekFrom::Start(pvh.metadata_descriptors[0].offset)).context(IoSnafu)?; // skip zero sheet
+        let metadata_descriptor = pvh.metadata_descriptors.first().context(MissingMetadataSnafu)?;
+
+        reader.seek(acid_io::SeekFrom::Start(metadata_descriptor.offset)).context(IoSnafu)?; // skip zero sheet
         reader.read_exact(&mut buf).context(IoSnafu)?;
         let (_, mah) = MetadataAreaHeader::parse(&buf).map_err(|e| Error::ParseError { error: e.to_string() })?;
         tracing::trace!(?mah);
 
         let mut metadata = String::new();
-        reader.seek(acid_io::SeekFrom::Start(pvh.metadata_descriptors[0].offset + mah.location_descriptors[0].data_area_offset)).context(IoSnafu)?; // skip zero sheet
-        reader.by_ref().take(mah.location_descriptors[0].data_area_size).read_to_string(&mut metadata).context(IoSnafu)?;
+        for locdesc in &mah.location_descriptors {
+            reader.seek(acid_io::SeekFrom::Start(metadata_descriptor.offset + locdesc.data_area_offset)).context(IoSnafu)?; // skip zero sheet
+            reader.by_ref().take(locdesc.data_area_size).read_to_string(&mut metadata).context(IoSnafu)?;
+        }
         tracing::debug!(%metadata);
 
-        let (trailing_garbage, metadata) = MetadataElements::parse(&metadata).unwrap();
+        let (trailing_garbage, metadata) = MetadataElements::parse(&metadata).map_err(|e| Error::ParseError { error: e.to_string() })?;
         tracing::debug!(?trailing_garbage, ?metadata);
 
-        let meta_root = ForceDeTypedMap::<String, MetadataRoot>::deserialize(&metadata).unwrap();
+        let meta_root = ForceDeTypedMap::<String, MetadataRoot>::deserialize(&metadata).context(SerdeSnafu)?;
         tracing::debug!(?meta_root);
 
+        ensure!(meta_root.0.len() == 1, MultipleVGsSnafu);
+        let (vg_name, vg_config) = meta_root.0.into_iter().next().unwrap();
 
-        Ok(Self { reader })
+        let pv_name = vg_config.physical_volumes.iter()
+            .find(|(_, v)| v.id.replace('-', "") == pvh.pv_ident)
+            .context(PVDoesntContainItselfSnafu)?
+            .0.clone();
+
+        Ok(Self { pvh, pv_name, vg_name, vg_config })
     }
-    pub fn foo(&mut self) -> Result<(), Error> {
-        /*let mut buf = [0u8; 512];
-        self.reader.read_exact(&mut buf)?; // first there is a 512-byte block of zeroes
-        self.reader.read_exact(&mut buf)?; // then the actual header
 
-        tracing::trace!(header = ?buf);
-        */
+    pub fn pv_id(&self) -> &str {
+        &self.pvh.pv_ident
+    }
+    pub fn pv_name(&self) -> &str {
+        &self.pv_name
+    }
 
+    pub fn vg_name(&self) -> &str {
+        &self.vg_name
+    }
+    pub fn vg_id(&self) -> &str {
+        &self.vg_config.id
+    }
 
+    pub fn lvs(&self) -> impl Iterator<Item=LV> {
+        self.vg_config.logical_volumes.iter().map(|(name, desc)| LV { name, desc })
+    }
+    pub fn open_lv<'a, T: Read + Seek>(&'a self, lv: &'a LV<'a>, reader: T) -> OpenLV<'a, T> {
+        OpenLV {
+            lv,
+            lvm: self,
+            reader,
 
-        Ok(())
+            position: 0,
+            current_segment_end: 0,
+        }
+    }
+
+    pub fn extent_size(&self) -> u64 {
+        self.vg_config.extent_size * 512
     }
 }
 
-#[derive(Debug)]
+
+
+pub struct OpenLV<'a, T> {
+    lv: &'a LV<'a>,
+    lvm: &'a Lvm2,
+    reader: T,
+
+    position: u64,
+    current_segment_end: u64,
+}
+impl<'a, T: Read + Seek> Read for OpenLV<'a, T> {
+    fn read(&mut self, mut buf: &mut [u8]) -> acid_io::Result<usize> {
+        if self.position == self.current_segment_end {
+            // trigger a seek to load the next segment
+            self.seek(SeekFrom::Current(0))?;
+        }
+
+        let max_read = self.current_segment_end - self.position;
+        if u64::try_from(buf.len()).unwrap() > max_read {
+            buf = &mut buf[..(max_read as usize)];
+        }
+        self.reader.read(buf)
+    }
+}
+impl<'a, T: Read + Seek> Seek for OpenLV<'a, T> {
+    fn seek(&mut self, pos: SeekFrom) -> acid_io::Result<u64> {
+        let pos = match pos {
+            SeekFrom::Start(x) => x,
+            SeekFrom::End(x) => ((self.lv.size_in_extents() * self.lvm.extent_size()) as i64 - x) as u64,
+            SeekFrom::Current(x) => ((self.position as i64) + x) as u64,
+        };
+        let target_extent = pos / self.lvm.extent_size();
+
+        let segment = self.lv.desc.segments.0.values().find(|x| x.extents().contains(&target_extent)).ok_or(acid_io::Error::new(acid_io::ErrorKind::Other, "no suitable segment found at this place"))?;
+        if segment.r#type != "striped" || segment.stripe_count != Some(1) {
+            return Err(acid_io::Error::new(acid_io::ErrorKind::Other, "segment is not linear"));
+        }
+
+        let offs_in_segment = pos - (segment.start_extent * self.lvm.extent_size());
+
+        let (pv, loc) = segment.stripes.as_ref().ok_or(acid_io::Error::new(acid_io::ErrorKind::Other, "segment has no stripes"))?;
+        if pv != self.lvm.pv_name() {
+            return Err(acid_io::Error::new(acid_io::ErrorKind::Other, "data is not on this PV"));
+        }
+
+        let mut seek_target = loc * self.lvm.extent_size() + offs_in_segment;
+        let mut found = false;
+        for dd in &self.lvm.pvh.data_descriptors {
+            if dd.size == 0 || dd.size > seek_target {
+                seek_target += dd.offset;
+                found = true;
+                break;
+            } else {
+                seek_target -= dd.size;
+            }
+        };
+        if !found {
+            return Err(acid_io::Error::new(acid_io::ErrorKind::Other, "data is beyond the end of this PV"));
+        }
+
+        // TODO: what is the purpose of this value if it's not needed for this?
+        //let pe_start = self.lvm.vg_config.physical_volumes.get(pv).unwrap().pe_start;
+
+        self.reader.seek(SeekFrom::Start(seek_target))?;
+
+        self.current_segment_end = segment.extents().end * self.lvm.extent_size();
+        self.position = pos;
+        Ok(pos)
+    }
+}
+
+pub struct LV<'a> {
+    name: &'a str,
+    desc: &'a LVDesc,
+}
+impl<'a> LV<'a> {
+    pub fn name(&self) -> &'a str {
+        &self.name
+    }
+    pub fn id(&self) -> &'a str {
+        &self.desc.id
+    }
+    pub fn size_in_extents(&self) -> u64 {
+        self.desc.segments.0.values().map(|x| x.start_extent + x.extent_count).max()
+            .expect("LV has no segments??")
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct ForceDeTypedMap<K, V>(BTreeMap<K, V>);
 impl<'de, K: Deserialize<'de> + Ord, V: Deserialize<'de>> Deserialize<'de> for ForceDeTypedMap<K, V> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -356,19 +483,21 @@ impl<'de, K: Deserialize<'de> + Ord, V: Deserialize<'de>> Deserialize<'de> for F
 }
 
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct MetadataRoot {
     pub id: String,
     pub extent_size: u64,
     pub physical_volumes: BTreeMap<String, PVDesc>,
     pub logical_volumes: BTreeMap<String, LVDesc>,
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct PVDesc {
     pub id: String,
     pub device: String,
+    pub pe_start: u64,
+    pub pe_count: u64,
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct LVDesc {
     pub id: String,
     pub status: Vec<String>,
@@ -379,7 +508,7 @@ pub struct LVDesc {
     #[serde(flatten)]
     pub segments: ForceDeTypedMap<String, LVSegmentDesc>,
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct LVSegmentDesc {
     pub start_extent: u64,
     pub extent_count: u64,
@@ -388,4 +517,9 @@ pub struct LVSegmentDesc {
     pub stripe_size: Option<usize>,
     pub stripes: Option<(String, u64)>,
     pub raid0_lvs: Option<Vec<String>>,
+}
+impl LVSegmentDesc {
+    pub fn extents(&self) -> Range<u64> {
+        self.start_extent .. (self.start_extent + self.extent_count)
+    }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -8,13 +8,15 @@ use positioned_io::ReadAt;
 use snafu::ResultExt;
 use tracing::Level;
 
+#[test]
 fn main() -> Result<(), snafu::Whatever> {
     tracing_subscriber::fmt().with_max_level(Level::TRACE).init();
     tracing::trace!("memes");
 
-    let mut f = File::open("/dev/mapper/mireska").whatever_context("")?;
+    let mut f = File::open("image.raw").whatever_context("")?;
+    //let mut f = File::open("/dev/mapper/mireska").whatever_context("")?;
     let lvm = Lvm2::open(&mut f).whatever_context("")?;
-    let lv = lvm.lvs().skip(3).next().unwrap();
+    let lv = lvm.lvs()/*.skip(3)*/.next().unwrap();
     tracing::info!("LV {}", lv.name());
     let mut olv = lvm.open_lv(&lv, &mut f);
 
@@ -29,13 +31,20 @@ fn main() -> Result<(), snafu::Whatever> {
     let mut options = Options::default();
     options.checksums = ext4::Checksums::Enabled;
     let e4 = ext4::SuperBlock::new_with_options(Wrapper(RefCell::new(olv)), &options).whatever_context("ext4")?;
-    let root = e4.root().whatever_context("root")?;
-    e4.walk(&root, "/", &mut |_, path, _, _| {
-        tracing::info!(%path);
-        Ok(true)
-    }).unwrap();
+    assert_eq!(read_file(&e4, "/testfile1"), "foo\n");
+    assert_eq!(read_file(&e4, "/testfile2"), "bar\n");
+
 
     Ok(())
+}
+
+fn read_file(e4: &ext4::SuperBlock<Wrapper<lvm2::OpenLV<&mut File>>>, name: &str) -> String {
+    let tf1 = e4.resolve_path(name).unwrap();
+    let inode = e4.load_inode(tf1.inode).unwrap();
+    let mut ino = e4.open(&inode).unwrap();
+    let mut s = String::new();
+    ino.read_to_string(&mut s).unwrap();
+    s
 }
 
 struct Wrapper<T>(RefCell<T>);
